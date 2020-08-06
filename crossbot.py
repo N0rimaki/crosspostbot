21e103a11d53389a5fecaa5e35c4ab32036ac6ff
Thomas <git@tomig.at>
2020-08-06 16:38:29 +0200

    refactor a bit

--- crossbot.py
+++ crossbot.py
@@ -1,67 +1,102 @@
+#!/usr/bin/python3
+__author__ = "u/wontfixit"
+__license__ = "GPL"
+__version__ = "1.0.0"
+
+
 import praw
 from datetime import datetime
 import configparser
 import logging as log
-
 now = datetime.now()
 timestamp = datetime.timestamp(now)
-timeMinusOneDay = timestamp-(24*60*60)
+
+
+#enable logging
+_DEBUG = True
+#will write changes to PROD if True
+_RUNPROD= False
+#days the script will look bakwards in time
+_DAYS = 2
+#calculate new timestamp
+timeMinusOneDay = timestamp-(24*60*60*_DAYS)
+
+
+if _RUNPROD == True:
+	#path too logfile (if run via crontab, path must be absolut)
+	_LOG_FILENAME = "/home/pi/crosspostbot/log_crossbot.log"
+	#config file path
+	_CONFIG_FILE = "/home/pi/crosspostbot/config.ini"
+else:
+	#path too logfile (if run via crontab, path must be absolut)
+	_LOG_FILENAME = "log_crossbot.log"
+	#config file path
+	_CONFIG_FILE = "config.ini"	
+
+
+if _DEBUG == True:
+	log.basicConfig( handlers=[
+            log.FileHandler(_LOG_FILENAME),
+            log.StreamHandler()],level=log.INFO,format='%(asctime)s ; %(levelname)s ; %(funcName)s() ; %(message)s')
 
 
 
-LOG_FILENAME = "/home/pi/crosspostbot/log_crossbot.txt"
 
-
-___debug___ = True
-___runprod___= False
-
-if ___debug___ == True:
-	log.basicConfig(filename=LOG_FILENAME,level=log.INFO,format='%(asctime)s : %(levelname)s : %(message)s')
-
-
-def work():
+def main():
 	log.info("Lastrun of the script")
+	try:
+		config = configparser.ConfigParser()
+		config.read(_CONFIG_FILE)
+		
+		_reddituser = config['DEFAULT']['_reddituser']
+		_subtocrosspost = config['DEFAULT']['_subtocrosspost']
+		_triggerwordscomments =config['DEFAULT']['_triggerwordscomments']
+		
+	except Exception as err:
+		log.error("Reading config didn't work {}".format(str(err)))
+		exit()
 	
-	config = configparser.ConfigParser()
-	config.read('/home/pi/crosspostbot/config.ini')
-	
-	_reddituser = config['DEFAULT']['_reddituser']
-	_subtocrosspost = config['DEFAULT']['_subtocrosspost']
-	_triggerwords =config['DEFAULT']['_triggerwords']
-	
+	log.info("Script will listen to r/{}".format(_subtocrosspost))	
+		
 	try:
 		_UA = 'crossbot by /u/['+_reddituser+']'
 		reddit = praw.Reddit("bot1",user_agent=_UA)
 		reddit.validate_on_submit=True	
 		
+		
+	except Exception as err:
+		log.error("reddit connection: {}".format(str(err)))
+		
+	try:	
 		result= reddit.redditor(_reddituser).comments.new(limit=None)
 		
 		for comment in result:
-			log.info("comment found id: {}".format(comment.id))
+			parent_id = comment.submission.id
+			log.info("comment found Pid: {} Cid:{}, no trigger".format(parent_id,comment.id))
 		
 			if comment.created_utc >= timeMinusOneDay:
-				if comment.body.strip().lower() in _triggerwords:
+				if comment.body.strip().lower() in _triggerwordscomments:
 					log.info("Trigger found: {}".format(str(comment.body.split("\n", 1)[0][:79])))
-					log.info("parent_id: {} comment_id: {}".format(comment.parent_id,comment))
+	
 					
-					
-					if ___runprod___ == True:
-						submission = reddit.submission(id=comment.parent_id.replace('t3_',''))
+					if _RUNPROD == True:
+						submission = reddit.submission(id=parent_id.replace('t3_',''))
 						cross_post = submission.crosspost(subreddit=_subtocrosspost,send_replies=False)
 						
 						edited_body = comment.body + "."
 						comment.edit(edited_body)
-						log.info("run IN Production")
+						
 					else:
 						log.info("run NOT IN Production")
 			else:
-				log.info("exit script")
+				log.info("finished and exit script")
 				exit()
+				
 	except Exception as err:
-		log.info("Error: {}".format(str(err)))
+		log.error("getting comments: {}".format(str(err)))
 
-	
-work()
+if __name__ == '__main__':	
+	main()
 
 
 
